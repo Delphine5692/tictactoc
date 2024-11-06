e94f536f44d43972e81bb7ee5c98f3d0a1ffc787
Delphine <delphine4492@gmail.com>
2024-11-06 14:53:18 +0100

    commentaire

--- monscript.js
+++ monscript.js
@@ -1,8 +1,15 @@
 
 var player = 1;
 
+var boxes = [];
+
+var gagnant = 0;
+
 function change_player()
 {
+    if (gagnant != 0)
+    {return;}
+
     if (player==1)
     {
         player=2;
@@ -23,6 +30,10 @@ function play(box)
 
     console.log("Player " + player + " vien de jouer la box " + box);
 
+    boxes[box]= player;
+
+    console.log (boxes);
+
     if (player==1)
         {
             $( "#b" + box).html("<img class='lettre_o' src='lettre_x.jpg'>");
@@ -33,7 +44,23 @@ function play(box)
     
         }
 
-    
+    if(boxes[1]==boxes[2]  && boxes[3] ==boxes[2] && boxes[2]  > 0) {
+      $( "#player" ).html("Player " + player + " a gagné");
+      gagnant=player
+
+     
+    }
+
+    if(boxes[4]==boxes[5]  && boxes[6] ==boxes[5] && boxes[5]  > 0) {
+      alert("Joueur " + player + " a Gagné")
+    }
+
+    if(boxes[7]==boxes[8]  && boxes[9] ==boxes[8] && boxes[8]  > 0) {
+      alert("Joueur " + player + " a Gagné")
+    }
+
+
+
 
     change_player();
 }
